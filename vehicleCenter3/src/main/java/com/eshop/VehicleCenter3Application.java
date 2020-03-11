67ab81740ec2c3cdf1622d51e3b542cfb34addda
dingyi <13787295142@163.com>
2020-03-12 00:30:22 +0800

    加入了跨域和静态资源配置，引入了spring-security,仍需完善

--- vehicleCenter3/src/main/java/com/eshop/VehicleCenter3Application.java
+++ vehicleCenter3/src/main/java/com/eshop/VehicleCenter3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@MapperScan(basePackages = {"com.eshop.dao","com.eshop.jt808.dao"})
+//@MapperScan(basePackages = {"com.eshop.dao","com.eshop.jt808.dao"}),如果在MybatisConfig里面没有配置就把这行加上
 @SpringBootApplication
 public class VehicleCenter3Application {
 
---
+++ vehicleCenter3/src/main/java/com/eshop/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.eshop.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${file.path}")
+    private String path;
+
+    @Value("${file.avatar}")
+    private String avatar;
+	
+	//跨域配置
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")	// 允许跨域访问的路径
+        .allowedOrigins("*")	// 允许跨域访问的源
+        .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")	// 允许请求方法
+        .maxAge(168000)	// 预检间隔时间
+        .allowedHeaders("*")  // 允许头部设置
+        .allowCredentials(true);	// 是否发送cookie
+    }
+
+	
+	//静态资源配置
+	/*
+	 * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	 * String avatarUtl = "file:" + avatar.replace("\\","/"); String pathUtl =
+	 * "file:" + path.replace("\\","/");
+	 * registry.addResourceHandler("/avatar/**").addResourceLocations(avatarUtl).
+	 * setCachePeriod(0);
+	 * registry.addResourceHandler("/file/**").addResourceLocations(pathUtl).
+	 * setCachePeriod(0); registry.addResourceHandler("/**").addResourceLocations(
+	 * "classpath:/META-INF/resources/").setCachePeriod(0); }
+	 */
+	
+	
+}
